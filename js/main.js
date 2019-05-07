be6eb75cb1cc453e338547d0f3733487e488578f
agentjongon <47461912+agentjongon@users.noreply.github.com>
2019-05-07 07:48:29 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
+// This is Jonathan, and here is the javascript that I used for my website, I wish I could've done more.
+
 function main() {
   $('.main').hide();
   $('.main').fadeIn(3000);
 }
 $(document).ready(main);
+
+//This was all I could create sadly.
